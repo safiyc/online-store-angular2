e85cdf30e20fb65f059df92f89fe9dcf69f52d6a
Elrey Belmonti and Linda Luu <linda@>
2017-10-30 08:53:52 -0700

    add a marketplace route component

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,6 +2,7 @@ import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { AboutComponent }   from './about/about.component';
+import { MarketplaceComponent }   from './marketplace/marketplace.component';
 
 const appRoutes: Routes = [
   {
@@ -11,7 +12,12 @@ const appRoutes: Routes = [
   {
     path: 'about',
     component: AboutComponent
+  },
+  {
+    path: 'marketplace',
+    component: MarketplaceComponent
   }
+
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
